49844f2dc898819ec6b01488361bc4cf85c73b16
Felipe Schwartz <felipeschwartz@gmail.com>
2026-05-26 14:10:13 -0300

    Atualização

--- src/main/java/felipeschwartz/com/github/Startup.java
+++ src/main/java/felipeschwartz/com/github/Startup.java
@@ -3,11 +3,13 @@ package felipeschwartz.com.github;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.felipeschwartz.com.github.SpringBootApplication;
+
 @SpringBootApplication
-public class RestWithSpringBootAndJavaErudioApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestWithSpringBootAndJavaErudioApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
