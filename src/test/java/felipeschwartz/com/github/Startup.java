91e63068961d0ce413f851eb4e8c9075ab04fc49
Felipe Schwartz <felipeschwartz@gmail.com>
2026-05-26 10:43:14 -0300

    Atualizando configurações do projeto

---
+++ src/test/java/felipeschwartz/com/github/Startup.java
@@ -0,0 +1,13 @@
+package felipeschwartz.com.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
