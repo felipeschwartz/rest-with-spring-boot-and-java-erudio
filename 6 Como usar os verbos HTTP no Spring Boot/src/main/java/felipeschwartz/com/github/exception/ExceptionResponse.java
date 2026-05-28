f47cd2c335af924014fd099449ebbc227d76e23c
Felipe Schwartz <felipeschwartz@gmail.com>
2026-05-28 08:46:17 -0300

    6 Como usar os verbos HTTP no Spring Boot

---
+++ 6 Como usar os verbos HTTP no Spring Boot/src/main/java/felipeschwartz/com/github/exception/ExceptionResponse.java
@@ -0,0 +1,6 @@
+package felipeschwartz.com.github.exception;
+
+
+import java.util.Date;
+
+public record ExceptionResponse(Date timestamp, String message, String details) {}
---
+++ 6 Como usar os verbos HTTP no Spring Boot/src/main/java/felipeschwartz/com/github/services/PersonServices.java
@@ -0,0 +1,70 @@
+package felipeschwartz.com.github.services;
+
+import felipeschwartz.com.github.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
+    public Person findById(String id) {
+        logger.info("Finding one person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAddress("Uberlandia - Minas Gerais - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person!");
+
+        return person;
+    }
+    public Person update(Person person) {
+        logger.info("Updating one person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deliting one person!");
+    }
+
+
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName" + (i+1));
+        person.setLastName("LastName" + (i+1));
+        person.setAddress("Some Address" + (i+1));
+        if (i % 2 == 0) {
+            person.setGender("Male");
+        } else {
+            person.setGender("Female");
+        }
+        return person;
+    }
+}
